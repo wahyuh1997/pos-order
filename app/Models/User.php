aecf9897b2c3e825a928bc8146e2a413822bd869
Andirifaldi <andirifaldi31@gmail.com>
2022-08-06 14:45:33 +0700

    upd202208061445

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getUpdatedAtAttribute()
+    {
+    return \Carbon\Carbon::parse($this->attributes['updated_at'])->format('Y-m-d H:i:s');
+    }
+
+    public function getCreatedAtAttribute()
+    {
+    return \Carbon\Carbon::parse($this->attributes['created_at'])->format('Y-m-d H:i:s');
+    }
 }
