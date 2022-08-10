fe6a5e1afde598f38692c76b04fb90bdca0a88a3
Andirifaldi <andirifaldi31@gmail.com>
2022-08-10 20:23:29 +0700

    upd202208102023

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -34,8 +34,17 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        // $this->reportable(function (Throwable $e) {
+        //     //
+        // });
+
+        $this->renderable(function (\Illuminate\Auth\AuthenticationException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Session Anda berakhir. silahkan login kembali!',
+                    'status'  => false
+                ], 401);
+            }
         });
     }
 }
