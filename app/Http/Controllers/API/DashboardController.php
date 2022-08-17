7a59140a4f6139bca58a1ea008418c9835f04454
Andirifaldi <andirifaldi31@gmail.com>
2022-08-17 21:12:19 +0700

    upd202208172112

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Pesanan;
+use App\Models\PesananDetail;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -12,6 +13,13 @@ class DashboardController extends Controller
     {
         $model = new Pesanan();
 
-        return $model->get_dashboard();
+        return $this->return_success('', $model->get_dashboard());
+    }
+
+    function get_report(Request $request)
+    {
+        $model = new PesananDetail();
+
+        return $model->report($request->from_date??\date('Y-m-d'), $request->thru_date??\date('Y-m-d'));
     }
 }
